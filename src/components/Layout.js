0132138576120ea1afe0613b62070495db1a8570
Matt <161460222+htnweb@users.noreply.github.com>
2024-11-27 13:31:21 +0000

    test analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/next';
 import React from 'react';
 
 
@@ -6,8 +5,7 @@ const Layout = ({children, className=" "}) => {
   return (
     <div className={`w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
         {children}
-        <Analytics/>
-        </div>
+      </div>
   )
 }
 
