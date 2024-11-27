2df6dc5219f23d5ce61fcde52f463c0758588ef8
Matt <161460222+htnweb@users.noreply.github.com>
2024-11-27 12:56:27 +0000

    test on navbar ml-3 to .ml-3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ const NavBar = () => {
 
                 
                 <button
-                onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} className= {`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} className= {`.ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
                     {
                         mode === 'dark' ?
                     <SunIcon className={'fill-dark'} />
