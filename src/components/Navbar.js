550ad9cde06a98aa1fc3bb819caef70ecd1b548b
Matt <161460222+htnweb@users.noreply.github.com>
2024-11-27 13:01:54 +0000

    Revert and update navbar to contain button title.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,8 +95,8 @@ const NavBar = () => {
                 ><LinkedInIcon/></motion.a>
 
                 
-                <button
-                onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} className= {`.ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                <button id='buttonTitle' title='Dark/Light Mode'
+                onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} className= {`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
                     {
                         mode === 'dark' ?
                     <SunIcon className={'fill-dark'} />
